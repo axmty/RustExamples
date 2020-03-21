4a6b98f624b321dca6bc245ee6a9156172d1d6a9
axmty <alexandre.metgy@gmail.com>
2020-03-21 17:45:50 +0100

    Activity in structures.rs: add rect_area function.

--- 3-custom_types/structures.rs
+++ 3-custom_types/structures.rs
@@ -10,15 +10,26 @@ struct Nil;
 // A tuple struct.
 struct Pair(i32, f32);
 
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
 }
 
 #[allow(dead_code)]
+#[derive(Debug)]
 struct Rectangle {
     top_left: Point,
     bottom_right: Point,
+}
+
+fn rect_area(rect: &Rectangle) -> f32 {
+    let Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: Point { x: right_edge, y: bottom_edge }
+    } = rect;
+
+    (right_edge - left_edge) * (top_edge - bottom_edge)
 }
 
 fn main() {
@@ -53,4 +64,11 @@ fn main() {
     // Tuple struct destructuring.
     let Pair(integer, decimal) = pair;
     println!("pair contains {:?} and {:?}", integer, decimal);
+
+    // Activity: calculate a Rectangle area.
+    let rect = Rectangle { 
+        top_left: Point { x: 2.3, y: 8.8},
+        bottom_right: Point { x: 5.3, y: 4.8 }
+    };
+    println!("{:?} area is {:.1}", rect, rect_area(&rect));
 }
