a72b31a8f3f9ead18d7541a5c8f07a3d74373014
axmty <alexandre.metgy@gmail.com>
2020-03-22 21:03:10 +0100

    Add struct destructuring example in match.rs.

--- 8-flow_of_control/match.rs
+++ 8-flow_of_control/match.rs
@@ -93,4 +93,24 @@ fn main() {
             println!("We added 10. 'mut_value': {:?}", m);
         }
     }
+
+
+    // Struct destructuring.
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo = Foo { x: (1, 2), y: 3 };
+
+    match foo {
+        Foo { x: (1, b), y }
+            => println!("First of x is 1, b = {}, y = {}", b, y),
+        Foo { y: 2, x: i }
+            => println!("y is 2, i = {:?}", i),
+        Foo { y, .. }
+            => println!("y = {}, we don't care about x", y),
+        // Error: pattern does not mention field 'x'.
+        // Foo { y } => println!("y = {}", y);
+    }
 }
