8c3b11b5ca9c5adf3310ef3f60bf1c892dd73f59
axmty <alexandre.metgy@gmail.com>
2020-03-22 21:05:56 +0100

    Add guards example in match.rs.

--- 8-flow_of_control/match.rs
+++ 8-flow_of_control/match.rs
@@ -113,4 +113,16 @@ fn main() {
         // Error: pattern does not mention field 'x'.
         // Foo { y } => println!("y = {}", y);
     }
+
+
+    // Guards.
+    let pair = (2, -2);
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first on is odd"),
+        _ => println!("No correlation..."),
+    }
 }
