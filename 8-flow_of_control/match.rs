aa8f44fe7b2a7f037a954a0f83c41e99f5c6b620
axmty <alexandre.metgy@gmail.com>
2020-03-22 20:17:56 +0100

    Add pointers/ref examples in match.rs.

--- 8-flow_of_control/match.rs
+++ 8-flow_of_control/match.rs
@@ -59,4 +59,38 @@ fn main() {
         Color::CMYK(c, m, y, k) =>
             println!("Cyan: {}, magenta: {}, yellow: {}, and key: {}!", c, m, y, k),
     }
+
+    // Assign a reference of type i32.
+    let reference = &4;
+    
+    // If the two & are dropped, then 'val' should be assigned a value.
+    match reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // To avoid the &, we can dereference 'reference' before matching.
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+
+    let _not_a_ref = 3;
+
+    // Same as 'let _is_a_ref = &3'.
+    let ref _is_a_ref = 3;
+
+
+    let value = 5;
+    let mut mut_value = 6;
+
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            *m += 10;
+            println!("We added 10. 'mut_value': {:?}", m);
+        }
+    }
 }
