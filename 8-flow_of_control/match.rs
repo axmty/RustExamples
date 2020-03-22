d1551953a0c4b6116d43aa943868b8e9dd189d1e
axmty <alexandre.metgy@gmail.com>
2020-03-22 19:05:51 +0100

    Add two other examples in match.rs: tuple destructuring and enums.

--- 8-flow_of_control/match.rs
+++ 8-flow_of_control/match.rs
@@ -1,3 +1,16 @@
+// Not all variants of Color are used.
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
 fn main() {
     let number = 13;
 
@@ -18,4 +31,32 @@ fn main() {
     };
 
     println!("{} -> {}", boolean, binary);
+
+    // Destructuring tuples.
+    let pair = (0, -2);
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (0, y) => println!("First is '0' and 'y' is {:?}", y),
+        (x, 0) => println!("'x' is {:?} and last is '0'", x),
+        _ => println!("It doesn't matter what they are"),
+    }
+
+    // Enums.
+    let color = Color::RGB(122, 17, 40);
+    println!("What color is it?");
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, and value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, and lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, and yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, and key: {}!", c, m, y, k),
+    }
 }
