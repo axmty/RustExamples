a85bce23724c2ca5ff9b1c8d567ec968852e1034
axmty <alexandre.metgy@gmail.com>
2020-07-19 13:39:40 +0200

    Create RustByExample folder to hold RustByExample snippets.

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    // Cannot assign twice, x is immutable.
+    // x = 6;
+    // println!("The value of x is: {}", x);
+}
